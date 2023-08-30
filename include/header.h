490e520ab90a418c851033349ee3e581521e864d
Sam <skalyaeve@gmail.com>
2023-08-30 05:01:03 +0200

    cleaning

--- include/header.h
+++ include/header.h
@@ -24,11 +24,11 @@ typedef struct	s_tab
 	int	o;
 	int	l;
 	int	p;
-}				t_tab;
+}		t_tab;
 
 typedef struct	s_hex_list
 {
-	char				c;
+	char			c;
 	struct s_hex_list	*prev;
 	struct s_hex_list	*next;
 }				t_hex_list;
@@ -37,45 +37,37 @@ typedef struct	s_type_list
 {
 	unsigned char		c;
 	unsigned char		a;
-	const char			*s;
-	int					i;
-	int					w;
+	const char		*s;
+	int			i;
+	int			w;
 	unsigned int		u;
 	unsigned int		x;
 	unsigned int		z;
 	unsigned long long	p;
 	unsigned int		q;
-	t_hex_list			*hex;
+	t_hex_list		*hex;
 }				t_type_list;
 
-int				ft_printf(const char *str, ...);
-void			ft_putnbr_fd(long long n, ssize_t *ret, int p, int o);
-ssize_t			convert_it(const char *str, int *i,
-			ssize_t *ret, va_list ap);
-ssize_t			finish_it(t_tab *molp, t_type_list *value, ssize_t *ret);
-ssize_t			get_final_lenght(t_tab *molp, t_type_list *value);
-void			avoid_convert(const char *str, int *i,
-			ssize_t *ret, t_tab *molp);
-void			print_hex(t_type_list *value, ssize_t *ret, t_tab *molp);
-ssize_t			count_nbr(ssize_t nbr);
-void			ft_putstr_fd(const char *s, ssize_t fd,
-			ssize_t *ret, t_tab *molp);
-ssize_t			hex_convert(t_tab *molp, t_type_list *value);
-ssize_t			fill_mo(t_tab *molp, const char *str,
-			int *i, va_list ap);
-ssize_t			fill_l(t_tab *molp, const char *str,
-			int *i, va_list ap);
-ssize_t			fill_p(t_tab *molp, const char *str, int *i, va_list ap);
-ssize_t			ft_strlen(const char *s);
-ssize_t			get_value(t_type_list *value, const char *str,
-			int *i, va_list ap);
-ssize_t			fill_hex_struct(unsigned long long nbr,
-			t_type_list *value, char *base, ssize_t *i);
-ssize_t			clean_up(t_tab *molp, t_type_list *value);
-void			clean_hex(t_type_list *value, t_hex_list *old);
-ssize_t			last_output(t_tab *molp, t_type_list *value, ssize_t *ret);
-void			init_values(t_type_list *value);
-void			fill_duxxp(t_type_list *value, const char *str,
-			int *i, va_list ap);
+int     ft_printf(const char *str, ...);
+void    ft_putnbr_fd(long long n, ssize_t *ret, int p, int o);
+ssize_t convert_it(const char *str, int *i, ssize_t *ret, va_list ap);
+ssize_t finish_it(t_tab *molp, t_type_list *value, ssize_t *ret);
+ssize_t get_final_lenght(t_tab *molp, t_type_list *value);
+void    avoid_convert(const char *str, int *i, ssize_t *ret, t_tab *molp);
+void    print_hex(t_type_list *value, ssize_t *ret, t_tab *molp);
+ssize_t count_nbr(ssize_t nbr);
+void    ft_putstr_fd(const char *s, ssize_t fd, ssize_t *ret, t_tab *molp);
+ssize_t hex_convert(t_tab *molp, t_type_list *value);
+ssize_t fill_mo(t_tab *molp, const char *str, int *i, va_list ap);
+ssize_t fill_l(t_tab *molp, const char *str, int *i, va_list ap);
+ssize_t fill_p(t_tab *molp, const char *str, int *i, va_list ap);
+ssize_t ft_strlen(const char *s);
+ssize_t get_value(t_type_list *value, const char *str, int *i, va_list ap);
+ssize_t fill_hex_struct(unsigned long long nbr, t_type_list *value, char *base, ssize_t *i);
+ssize_t clean_up(t_tab *molp, t_type_list *value);
+void    clean_hex(t_type_list *value, t_hex_list *old);
+ssize_t last_output(t_tab *molp, t_type_list *value, ssize_t *ret);
+void    init_values(t_type_list *value);
+void    fill_duxxp(t_type_list *value, const char *str, int *i, va_list ap);
 
 #endif
